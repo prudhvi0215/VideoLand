9e95eb348c2e7bb3ae808972676e2914fd930f07
Prudhvi Raj Valapolu <prudhviraj3099@gmail.com>
2022-10-28 14:01:36 +0530

    API-Leak-Fix

--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -2,12 +2,14 @@ const axios = require("axios");
 
 const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
+const API_KEY = import.meta.env.REACT_APP_RAPID_API_KEY;
+
 const options = {
   params: {
     maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": "c674a5d10cmsh7d7fba21856d373p196d8fjsn5e445316ca3f",
+    "X-RapidAPI-Key": API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
